04907c3c9b0d8c0bd5222582a46cd6041efff301
Ryan Ferguson <nythrye@gmail.com>
2025-03-08 17:18:34 -0800

    fixed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('./app');
 
 // START SERVER LISTENING
-const port = 3000;
+const port = 8080;
 app.listen(process.env.PORT || port, () => {
   console.log(`Server running on port ${port}/`)
 });
