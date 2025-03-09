f1c7ef212fee11d63ddbc1e7888dd1901925e9f7
Ryan Ferguson <nythrye@gmail.com>
2025-03-08 17:12:13 -0800

    simplify app.js

--- app.js
+++ app.js
@@ -1,31 +1,14 @@
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const routes = require('./server/routes/routes.js');
-
-//flash messages dependencies:
-const session = require('express-session');
-const cookieParseer = require('cookie-parser');
-const flash = require('connect-flash');
-
+const path = require('path');
 const app = express();
+const port = 3000;
 
-// CONFIGURE
-app.use(express.urlencoded( { extended: true } ));
-app.use(express.static('public'));
-app.use(expressLayouts);
+app.use(express.static(path.join(__dirname)));
 
-app.use(cookieParseer('MongoBlogExampleSecure'));
-app.use(
-  session({
-    resave: true,
-    saveUninitialized: true,
-    secret: "who is yash?",
-    cookie: { secure: false, maxAge: 14400000 },
-  })
-)
-app.use(flash());
-app.set('layout', './layouts/main');  //main.ejs is our main template
-app.set("view engine", "ejs");
-app.use('/', routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
